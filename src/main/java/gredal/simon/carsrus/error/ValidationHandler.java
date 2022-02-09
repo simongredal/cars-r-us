869b8e08d7ef64585edd45ce932e88f882156ba0
Simon Gredal <simon.gredal@icloud.com>
2022-02-09 15:55:55 +0100

    Use more appropriate ResponseStatusException types

--- src/main/java/gredal/simon/carsrus/error/ValidationHandler.java
+++ src/main/java/gredal/simon/carsrus/error/ValidationHandler.java
@@ -4,10 +4,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import javax.servlet.http.HttpServletRequest;
@@ -16,14 +17,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class ValidationHandler extends ResponseEntityExceptionHandler {
 
     //This provides a decent error response when spring validation fails
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
-                                                                  HttpHeaders headers, HttpStatus status, WebRequest request) {
-
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, Object> errorBody = new HashMap<>();
         errorBody.put("path", ((ServletWebRequest)request).getRequest().getRequestURI());
         errorBody.put("status","400");
@@ -37,13 +36,15 @@ public class ValidationHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<Object>(errorBody, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler(Client4xxException.class)
-    public ResponseEntity<Map<String, String>> handleException(HttpServletRequest request, Client4xxException e) {
+    @ExceptionHandler(ResponseStatusException.class)
+    public ResponseEntity<Map<String, String>> handleException(HttpServletRequest request, ResponseStatusException ex) {
         Map<String, String> errorResponse = new HashMap<>();
         errorResponse.put("path", request.getRequestURI());
-        errorResponse.put("error", e.getLocalizedMessage());
-        errorResponse.put("status", ""+e.getStatus().value());
-        return new ResponseEntity<>(errorResponse, e.getStatus());
+        errorResponse.put("status", String.valueOf(ex.getStatus().value()));
+        errorResponse.put("exception", ex.getClass().getSimpleName());
+        errorResponse.put("reason", ex.getReason());
+
+        return new ResponseEntity<>(errorResponse, ex.getStatus());
     }
 
 }
