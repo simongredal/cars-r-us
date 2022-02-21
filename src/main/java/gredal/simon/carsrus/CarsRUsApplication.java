9bf7c50db19d4631bbd95f2e5182ff7da494fa12
Simon Gredal <simon.gredal@icloud.com>
2022-02-21 09:45:26 +0100

    Fix index.html not working in jar file

--- src/main/java/gredal/simon/carsrus/CarsRUsApplication.java
+++ src/main/java/gredal/simon/carsrus/CarsRUsApplication.java
@@ -2,30 +2,13 @@ package gredal.simon.carsrus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 
 @SpringBootApplication
-@RestController
+@Controller
 public class CarsRUsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarsRUsApplication.class, args);
-    }
-
-    private String html;
-
-    @GetMapping
-    public String index() {
-        if (html == null) {
-            InputStream resource = ClassLoader.getSystemResourceAsStream("static/index.html");
-            assert resource != null;
-            BufferedReader reader = new BufferedReader(new InputStreamReader(resource));
-            html = String.join("", reader.lines().toList());
-        }
-        return html;
     }
 }
