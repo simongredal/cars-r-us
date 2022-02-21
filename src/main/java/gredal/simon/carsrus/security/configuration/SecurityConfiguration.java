7b3ac13b300d5b1c49fe5cba208b24a8ee8c05a0
Simon Gredal <simon.gredal@icloud.com>
2022-02-21 08:59:09 +0100

    Add swagger docs

--- src/main/java/gredal/simon/carsrus/security/configuration/SecurityConfiguration.java
+++ src/main/java/gredal/simon/carsrus/security/configuration/SecurityConfiguration.java
@@ -1,4 +1,4 @@
-package gredal.simon.carsrus.security.config;
+package gredal.simon.carsrus.security.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,7 +9,7 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 /*  THIS CONFIGURATION DISABLES SECURITY. THIS FILE WILL BE REPLACED WITH ANOTHER LATER */
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
