2efff1be979652b79a13808f370cd73844564a16
Simon Gredal <simon.gredal@icloud.com>
2022-02-21 08:59:33 +0100

    Create an index page and link to the swagger docs

--- src/main/java/gredal/simon/carsrus/CarsRUsApplication.java
+++ src/main/java/gredal/simon/carsrus/CarsRUsApplication.java
@@ -2,10 +2,30 @@ package gredal.simon.carsrus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 
 @SpringBootApplication
+@RestController
 public class CarsRUsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarsRUsApplication.class, args);
+    }
+
+    private String html;
+
+    @GetMapping
+    public String index() {
+        if (html == null) {
+            InputStream resource = ClassLoader.getSystemResourceAsStream("static/index.html");
+            assert resource != null;
+            BufferedReader reader = new BufferedReader(new InputStreamReader(resource));
+            html = String.join("", reader.lines().toList());
+        }
+        return html;
     }
 }
