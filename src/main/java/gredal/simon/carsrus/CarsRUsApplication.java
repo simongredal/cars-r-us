bf34447831d7804fb456d05759c22cfcad0a25ac
Simon Gredal <simon.gredal@icloud.com>
2022-03-13 23:55:02 +0100

    Refactor index page
    Index Page is now served as a thymeleaf template from a separate controller.
    
    A plugin has been added to show build time and commit id on the index page.

--- src/main/java/gredal/simon/carsrus/CarsRUsApplication.java
+++ src/main/java/gredal/simon/carsrus/CarsRUsApplication.java
@@ -2,11 +2,8 @@ package gredal.simon.carsrus;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
-@Controller
 public class CarsRUsApplication {
     public static void main(String[] args) {
         SpringApplication.run(CarsRUsApplication.class, args);
